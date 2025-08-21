f349f00c368a4938384b6e8c108916b5b08a351d
sidydenon <sidydenon63@icloud.com>
2025-08-21 01:54:55 +0100

    version 1.1

--- 04_Développement/frontend/TTM/src/Accueil.jsx
+++ 04_Développement/frontend/TTM/src/Accueil.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 function Accueil() {
+  const HERO = "/assets/accueil.png"; // or .png/.webp — exact name & case
+
   return (
-    <div>
-        <div className=" h-screen bg-[url(./assets/accueil.png)] bg-cover  bg-center top-15  w-screen fixed ">
+    <div
+       className="min-h-screen w-full flex items-center justify-center fixed"
+      style={{
+        backgroundImage: `linear-gradient(rgba(0,0,0,.45), rgba(0,0,0,.45)), url(${HERO})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}>
         <div className="bg-[#00000051] w-full h-full ">
         </div>
-        </div> 
     </div>
   )
 }

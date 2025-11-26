abcae0c812da96d2b8e468e1753329b63afb8127
sidydenon <sidydenon63@icloud.com>
2025-11-26 04:55:19 +0100

    j´ai ajouter  .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,15 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
-dotenv.config();
+// Charger dotenv seulement en local
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD, // <-- IMPORTANT : DB_PASSWORD
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
